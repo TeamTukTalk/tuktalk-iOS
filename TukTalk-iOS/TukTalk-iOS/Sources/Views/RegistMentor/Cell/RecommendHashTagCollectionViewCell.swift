61dff0065bb48c4845efb9770eed831867b7ae73
Hansangjin98 <onetable98@naver.com>
2021-12-06 20:37:13 +0900

    [REFACTOR] 추천 해시태그 컬렉션뷰 셀 코드 수정

--- TukTalk-iOS/TukTalk-iOS/Sources/Views/RegistMentor/Cell/RecommendHashTagCollectionViewCell.swift
+++ TukTalk-iOS/TukTalk-iOS/Sources/Views/RegistMentor/Cell/RecommendHashTagCollectionViewCell.swift
@@ -41,7 +41,7 @@ class RecommendHashTagCollectionViewCell: UICollectionViewCell {
     }
     
     static func fittingSize(availableHeight: CGFloat, title: String?) -> CGSize {
-        let cell = CompanyCollectionViewCell()
+        let cell = RecommendHashTagCollectionViewCell()
         cell.configure(title: title)
         let targetSize = CGSize(width: UIView.layoutFittingCompressedSize.width, height: availableHeight)
         return cell.contentView.systemLayoutSizeFitting(targetSize, withHorizontalFittingPriority: .fittingSizeLevel, verticalFittingPriority: .required)
